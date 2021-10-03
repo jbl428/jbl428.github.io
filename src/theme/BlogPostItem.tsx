0fd3ec48a3494fcc689e272a7b7a7d774aa39cb2
Jake Son <sonjeabin@gmail.com>
2021-10-03 12:30:09 +0900

    Apply utterances

---
+++ src/theme/BlogPostItem.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react';
+import OriginalBlogPostItem from '@theme-original/BlogPostItem';
+import useThemeContext from '@theme/hooks/useThemeContext';
+
+const utterancesSelector = 'iframe.utterances-frame';
+
+ function BlogPostItem(props) {
+   const {isDarkTheme} = useThemeContext();
+   const utterancesTheme = isDarkTheme ? 'github-dark' : 'github-light';
+   const containerRef = useRef(null);
+
+   useEffect(() => {
+     if (!props.isBlogPostPage) return;
+
+     const utterancesEl = containerRef.current.querySelector(utterancesSelector);
+
+     const createUtterancesEl = () => {
+       const script = document.createElement('script');
+
+       script.src = 'https://utteranc.es/client.js';
+       script.setAttribute('repo', 'jbl428/jbl428.github.io');
+       script.setAttribute('issue-term', 'pathname');
+       script.setAttribute('label', 'comment');
+       script.setAttribute('theme', utterancesTheme);
+       script.crossOrigin = 'anonymous';
+       script.async = true;
+
+       containerRef.current.appendChild(script);
+     };
+
+     const postThemeMessage = () => {
+       const message = {
+         type: 'set-theme',
+         theme: utterancesTheme,
+       };
+
+       utterancesEl.contentWindow.postMessage(message, 'https://utteranc.es');
+     };
+
+     utterancesEl ? postThemeMessage() : createUtterancesEl();
+   }, [utterancesTheme]);
+
+  return (
+    <>
+      <OriginalBlogPostItem {...props} />
+      {props.isBlogPostPage && <div ref={containerRef} />}
+    </>
+  );
+}
+
+export default BlogPostItem;
